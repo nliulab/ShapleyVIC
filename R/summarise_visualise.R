1ff207d7bd81a46475d76c03c9a3999998163d4a
Yilin Ning <yilinning@Yilins-Mac-mini.local>
2021-08-27 12:25:37 +0800

    properly calls python functions

--- R/summarise_visualise.R
+++ R/summarise_visualise.R
@@ -31,7 +31,7 @@ compute_meta_interval <- function(val, val_sd) {
     hetero_i2 = summ$I2$TE, hetero_i2_lower = summ$I2$lower,
     hetero_i2_upper = summ$I2$upper)
 }
-#' Summarise ShapleyVIC values
+#' Summarize ShapleyVIC values
 #' @param val A vector of ShapleyVIC values from all models evaluated.
 #' @param val_sd A vector of standard deviation of ShapleyVIC values from all
 #'   models evaluated. Must be the same length as \code{val}. If not available,
@@ -126,6 +126,7 @@ draw_bars <- function(val, val_sd = NULL, val_lower = NULL, val_upper = NULL,
                         panel.background = element_blank(),
                         axis.line.x = element_line(colour = "black"),
                         axis.ticks.y = element_blank())
+  x_lab <- "Average model reliance (>0 suggests importance)\nwith 95% predicion interval"
   if (!is.null(sign_vec)) {
     dt$sign_vec_factor <- factor(sign_vec)
     color_vec <- set_color_template(sign_vec = sign_vec,
@@ -137,7 +138,7 @@ draw_bars <- function(val, val_sd = NULL, val_lower = NULL, val_upper = NULL,
       geom_bar(stat = "identity") +
       common_theme +
       coord_flip() +
-      labs(x = "", y = "", title = title, subtitle = subtitle) +
+      labs(x = "", y = x_lab, title = title, subtitle = subtitle) +
       theme(legend.position = "none") +
       scale_fill_manual(values = color_vec)
   } else {
@@ -147,7 +148,7 @@ draw_bars <- function(val, val_sd = NULL, val_lower = NULL, val_upper = NULL,
       geom_bar(stat = "identity") +
       common_theme +
       coord_flip() +
-      labs(x = "", y = "", title = title, subtitle = subtitle) +
+      labs(x = "", y = x_lab, title = title, subtitle = subtitle) +
       theme(legend.position = "none")
   }
   if (!is.null(labels)) {
@@ -204,15 +205,20 @@ rank_variables <- function(val, val_sd = NULL, ties.method = "min") {
 #' @param val Model reliance.
 #' @param loss_ratio Ratio of loss to optimal loss, to determine color.
 #' @import tidyverse
-prep_data_zebra <- function(var_names, val, loss_ratio, my_width = 0.35) {
-  df <- data.frame(x = var_names, y = val, loss_ratio = loss_ratio)
-  p <- df %>% ggplot(aes(x = x, y = y)) + geom_violin(trim = TRUE)
+#' @importFrom rlang .data
+#' @importFrom purrr map_df
+prep_data_zebra <- function(var_names, val, perf_metric,
+                            perf_metric_breaks = c(1.01, 1.02, 1.03, 1.04),
+                            my_width = 0.35) {
+  df <- data.frame(x = var_names, y = val, perf_metric = perf_metric)
+  p <- df %>% ggplot(aes_string(x = "x", y = "y")) + geom_violin(trim = TRUE)
   # This is all you need for the fill:
   vl_fill <- data.frame(ggplot_build(p)$data) %>%
-    group_by(x) %>%
-    mutate(width = violinwidth / max(violinwidth)) %>%
+    group_by(.data$x) %>%
+    mutate(width = .data$violinwidth / max(.data$violinwidth)) %>%
     ungroup() %>%
-    mutate(xnew = x - my_width * width, xend = x + my_width * width)
+    mutate(xnew = .data$x - my_width * .data$width,
+           xend = .data$x + my_width * .data$width)
   # color by population
   x_lvls <- levels(df$x)
   vl_fill2 <- do.call("rbind", lapply(unique(vl_fill$x), function(xi) {
@@ -223,30 +229,50 @@ prep_data_zebra <- function(var_names, val, loss_ratio, my_width = 0.35) {
     for (i in 1:n_i) {
       rows <- which(dat_i$y < vl_fill_i$ymax[i])
       if (length(rows) > 0) {
-        y_color[i] <- mean(dat_i$loss_ratio[rows])
+        y_color[i] <- mean(dat_i$perf_metric[rows])
         dat_i <- dat_i[-rows, ]
       }
     }
-    vl_fill_i <- vl_fill_i %>%
-      mutate(y_color = y_color) %>%
-      fill(y_color, .direction = "updown")
+    vl_fill_i$y_color <- y_color
+    vl_fill_i <- fill(data = vl_fill_i, .data$y_color, .direction = "updown")
     vl_fill_i
   })) %>%
-    mutate(y_color_cat = cut(y_color, right = FALSE,
-                             breaks = c(-Inf, 1.01, 1.02, 1.03, 1.04, Inf),
-                             labels = paste0("<", 1 + (1:5) / 100)))
+    mutate(y_color_cat = cut(.data$y_color, right = FALSE,
+                             breaks = unique(c(-Inf, perf_metric_breaks, Inf))))
   # Bit convoluted for the outline, need to be rearranged: the order matters
   vl_poly <- vl_fill2 %>%
-    select(xnew, xend, y, group) %>%
-    pivot_longer(-c(y, group), names_to = "oldx", values_to = "x") %>%
-    arrange(y) %>%
+    select(.data$xnew, .data$xend, .data$y, .data$group) %>%
+    pivot_longer(-c(.data$y, .data$group), names_to = "oldx", values_to = "x") %>%
+    arrange(.data$y) %>%
     split(., .$oldx) %>%
-    purrr::map(., function(x) {
+    purrr::map_df(., function(x) {
       if (all(x$oldx == "xnew")) x <- arrange(x, desc(y))
       x
-    }) %>%
-    bind_rows()
+    })
   list(df_zebra_color = vl_fill2, df_violin_shape = vl_poly)
+}
+#' @import ggplot2
+#' @import ggpubr
+make_violin_legend <- function(perf_metric_breaks, col_vec) {
+  n_brk <- length(perf_metric_breaks)
+  brk_diff <- diff(perf_metric_breaks)
+  y_color <- c(perf_metric_breaks[1] - brk_diff[1], perf_metric_breaks,
+               perf_metric_breaks[n_brk] + brk_diff[n_brk - 1])
+  n_mid <- round(median(1:(n_brk + 1)))
+  p_legend <- ggplot(data = data.frame(x = seq_along(y_color),
+                                       y = seq_along(y_color),
+                                       y_color = y_color),
+                     aes_string(x = "x", y = "y", fill = "y_color")) +
+    geom_tile() +
+    # col_vec is for discrete version. it is in reverse direction of continuous version
+    scale_fill_gradient2(low = col_vec[n_brk + 1], high = col_vec[1],
+                         mid = col_vec[n_mid], midpoint = median(y_color),
+                         labels = c("Lower", rep("", n_brk), "Higher")) +
+    theme(legend.position = "bottom") +
+    guides(fill = guide_colorbar(title = "Model performance", title.vjust = 1,
+                                 ticks = FALSE, barwidth = grid::unit(0.5, "npc"),
+                                 barheight = grid::unit(0.5, "lines")))
+  ggpubr::get_legend(p_legend)
 }
 #' Draw colored violin plot for model reliance.
 #' @param var_names Factor of variable names. Must be a factor.
@@ -258,12 +284,14 @@ prep_data_zebra <- function(var_names, val, loss_ratio, my_width = 0.35) {
 #' @import RColorBrewer
 #' @import ggpubr
 #' @export
-draw_violins <- function(var_names, val, loss_ratio, title = "", colored = TRUE,
-                         my_width = 0.35) {
+draw_violins <- function(var_names, val, perf_metric, smaller_is_better = TRUE,
+                         title = "", colored = TRUE,
+                         my_width = 0.35,
+                         perf_metric_breaks = c(1.01, 1.02, 1.03, 1.04)) {
   # First prepare the list of two data.frames for the shape and color strips
   # of violins:
   df_violin_list <- prep_data_zebra(var_names = var_names,
-                                    val = val, loss_ratio = loss_ratio)
+                                    val = val, perf_metric = perf_metric)
   x_lvls <- levels(var_names)
   # The shape of violins are always plotted:
   p <- ggplot() +
@@ -278,25 +306,22 @@ draw_violins <- function(var_names, val, loss_ratio, title = "", colored = TRUE,
           panel.background = element_blank(),
           axis.line.x = element_line(colour = "black"),
           axis.ticks.y = element_blank()) +
-    # labs(x = "", y = "", title = title)
-    labs(x = "", y = "Model reliance", title = title)
+    labs(x = "", y = "Model reliance (>0 suggests importance)", title = title)
   # If colored, also add color strips:
   if (colored) {
+    n_cats <- length(perf_metric_breaks) + 1
+    col_vec <- RColorBrewer::brewer.pal(n = n_cats + 1, name = "Blues")[-1]
+    if (all(perf_metric_breaks >= 1)) col_vec <- rev(col_vec)
     # First create a legend for fill, which looks nicer than default legend for
     # color:
-    p_color_legend <- p +
-      geom_tile(data = df_violin_list$df_zebra_color,
-                aes_string(x = "xnew", y = "y", fill = "y_color_cat")) +
-      scale_fill_manual(values = rev(RColorBrewer::brewer.pal(n = 6, name = "Blues")[-1])) +
-      guides(fill = guide_legend(title = "Model loss (as ratio to minimum loss)")) +
-      theme(legend.position = "bottom")
-    color_legend <- ggpubr::get_legend(p_color_legend)
+    color_legend <- make_violin_legend(perf_metric_breaks = perf_metric_breaks,
+                                       col_vec = col_vec)
     # Then make the colored violin plots:
     p <- p +
       geom_segment(data = df_violin_list$df_zebra_color,
                    aes_string(x = "xnew", xend = "xend", y = "y", yend = "y",
                               color = "y_color_cat")) +
-      scale_color_manual(values = rev(RColorBrewer::brewer.pal(n = 6, name = "Blues")[-1])) +
+      scale_color_manual(values = col_vec) +
       theme(legend.position = "none")
     # Finally, combine plot with legend:
     p <- ggpubr::ggarrange(p, legend.grob = color_legend, legend = "bottom")
